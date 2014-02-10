e42f354545fa57eb8386a6f8c8e5e5f599a2e1b1
Dorian Burihabwa <dorian@burihabwa.com>
2014-02-10 19:23:06 +0100

    Refactoring the server

--- FTP/src/main/java/car/ftp/Main.java
+++ FTP/src/main/java/car/ftp/Main.java
@@ -1,65 +1,18 @@
 package main.java.car.ftp;
 
-import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import main.java.car.ftp.utils.FtpWorker;
 
 public class Main {
-	private static Logger logger = Logger.getAnonymousLogger();
-	public static int port;
-	public static File directory;
-
-	public static void initialize(int portNumber, String directoryPath) {
-		if (portNumber < 1024) {
-			throw new IllegalArgumentException(
-					"This server should not run on a port lower than 1024!");
-		}
-		if (directoryPath == null) {
-			throw new IllegalArgumentException(
-					"The directory argument cannot be null!");
-		}
-		port = portNumber;
-		directory = new File(directoryPath);
-		if (!directory.exists()) {
-			throw new IllegalArgumentException("The directory " + directoryPath
-					+ " does not exist!");
-		}
-		if (!directory.canRead() || !directory.canExecute()) {
-			throw new IllegalArgumentException(
-					"The content of the server directory cannot be read!");
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		if (args.length != 2) {
 			System.out.println("java FTP.jar");
 			System.exit(0);
 		}
-		initialize(Integer.parseInt(args[0]), args[1]);
-		ServerSocket socket = null;
-		BufferedInputStream bis = null;
-		try {
-			socket = new ServerSocket(1024);
-			while (true) {
-				Socket clientSocket = socket.accept();
-				FtpWorker worker = new FtpWorker(clientSocket);
-				Thread t = new Thread(worker);
-				t.start();
-			}
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, e.toString());
-			System.exit(2);
-		} finally {
-			if (bis != null)
-				bis.close();
-			if (!socket.isClosed())
-				socket.close();
-		}
+		int port = Integer.parseInt(args[0]);
+		String directoryPath = args[1];
+		Server server = Server.getServer(port, directoryPath);
+		System.out.println("Starting server on port: " + port);
+		server.start();
+		System.out.println("Stopping server!");
 	}
 }
--- FTP/src/main/java/car/ftp/Server.java
+++ FTP/src/main/java/car/ftp/Server.java
@@ -5,9 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,42 +15,46 @@ public class Server {
 	private ServerSocket socket;
 	/** Port number for the server */
 	private int port;
-	/** Number of workers available to the server */
-	private AtomicInteger workers;
-	/** Workers at server's disposal */
-	private List<FtpWorker> ftpWorkers;
 	/** Root directory for the server */
 	private File rootDirectory;
 
 	private Logger logger = Logger.getAnonymousLogger();
 
+	public static Server SERVER = null;
+
 	private Server() {
 		;
 	}
 
-	public Server(int port, int workers, String directoryPath) {
-		this.workers = new AtomicInteger(workers);
-		this.ftpWorkers = new ArrayList<FtpWorker>(workers);
-		this.rootDirectory = new File(directoryPath);
-		if (!rootDirectory.exists()) {
+	public static Server getServer(int port, String directoryPath) {
+		if (SERVER != null) {
+			return SERVER;
+		}
+		SERVER = new Server();
+		SERVER.rootDirectory = new File(directoryPath);
+		SERVER.port = port;
+		if (!SERVER.rootDirectory.exists()) {
 			throw new IllegalArgumentException("The directory does not exist!");
 		}
-		if (!rootDirectory.isDirectory()) {
+		if (!SERVER.rootDirectory.isDirectory()) {
 			throw new IllegalArgumentException(
 					"The directory path does not point to a directory!");
 		}
-		if (!rootDirectory.canRead() || !rootDirectory.canWrite()
-				|| !rootDirectory.canExecute()) {
+		if (!SERVER.rootDirectory.canRead() || !SERVER.rootDirectory.canWrite()
+				|| !SERVER.rootDirectory.canExecute()) {
 			throw new IllegalArgumentException(
 					"The server does run with the rights to read, write and execute on the directory!");
 		}
+		return SERVER;
 	}
 
 	public void start() throws IOException {
-		ServerSocket socket = null;
+		System.out.println("The server is now starting!");
+		this.socket = null;
 		BufferedInputStream bis = null;
 		try {
-			socket = new ServerSocket(1024);
+			socket = new ServerSocket(this.port);
+			logger.log(Level.INFO, "The server will now wait for connections on port " + this.port);
 			while (true) {
 				Socket clientSocket = socket.accept();
 				FtpWorker worker = new FtpWorker(clientSocket);
@@ -69,5 +70,21 @@ public class Server {
 			if (!socket.isClosed())
 				socket.close();
 		}
+	}
+
+	public ServerSocket getSocket() {
+		return socket;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public File getRootDirectory() {
+		return rootDirectory;
+	}
+
+	public static Server getSERVER() {
+		return SERVER;
 	}
 }
