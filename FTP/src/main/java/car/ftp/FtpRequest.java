740380233b1d99a547eeafd13f6cb69d330c673f
Dorian Burihabwa <dorian@burihabwa.com>
2014-02-18 02:39:22 +0100

    [DOC] Documenting the FtpRequest class

--- FTP/src/main/java/car/ftp/FtpRequest.java
+++ FTP/src/main/java/car/ftp/FtpRequest.java
@@ -27,6 +27,13 @@ import java.util.logging.Logger;
 import main.java.car.ftp.exceptions.UnsupportedCommandException;
 import main.java.car.ftp.utils.CommandParser;
 
+/**
+ * Class handling all commands by dispatching to the right handlers and
+ * providing small utility methods to communicate with the client.
+ * 
+ * @author dorian
+ * 
+ */
 public class FtpRequest {
 	private Logger logger = Logger.getAnonymousLogger();
 	private ClientSession clientSession;
@@ -45,6 +52,14 @@ public class FtpRequest {
 		this.clientSession = clientSession;
 	}
 
+	/**
+	 * Returns the output stream that allows to write responses to the client.
+	 * 
+	 * @return the output stream on the command socket
+	 * @throws IOException
+	 *             If an error occurs when trying to recover the output stream
+	 *             on the command socket
+	 */
 	public DataOutputStream getOutputStream() throws IOException {
 		if (dos == null)
 			this.dos = new DataOutputStream(this.clientSession
@@ -52,14 +67,36 @@ public class FtpRequest {
 		return this.dos;
 	}
 
+	/**
+	 * Returns the data socket transparently regardless of the mode (active or
+	 * passive).
+	 * 
+	 * @return
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
 	public Socket getDataSocket() throws UnknownHostException, IOException {
 		return clientSession.getDataSocket();
 	}
 
+	/**
+	 * Returns the type last requested by the client.
+	 * 
+	 * @return last type requested by the client
+	 */
 	public boolean getBinaryFlag() {
 		return this.binaryFlag;
 	}
 
+	/**
+	 * Parses a command and dispatches the work to the matching method. In case
+	 * of error, the method throws an {@link UnsupportedCommandException}. which
+	 * should be caught by the object that has called the method.
+	 * 
+	 * @param commandString
+	 *            The command as string
+	 * @throws UnsupportedCommandException
+	 */
 	public void processRequest(final String commandString)
 			throws UnsupportedCommandException {
 		Command command = CommandParser.parse(commandString);
@@ -188,6 +225,14 @@ public class FtpRequest {
 		dos.writeBytes("230 Password accepted, please proceed!\n");
 	}
 
+	/**
+	 * Sets the server in passive mode.
+	 * 
+	 * @param argument
+	 *            unused.
+	 * @throws IOException
+	 *             If an error occurs while trying write on the command socket.
+	 */
 	protected void processPASV(final String argument) throws IOException {
 		ServerSocket ss = clientSession.setPassiveMode();
 		String address = "";
@@ -204,6 +249,14 @@ public class FtpRequest {
 		dos.writeBytes("227 " + message + "\n");
 	}
 
+	/**
+	 * Save the address and port to be used for a future command.
+	 * 
+	 * @param argument
+	 *            concatenated address and port number
+	 * @throws IOException
+	 *             If an error occurs while trying write on the command socket
+	 */
 	protected void processPORT(final String argument) throws IOException {
 		dos = getOutputStream();
 		clientSession.setActiveMode();
@@ -270,6 +323,15 @@ public class FtpRequest {
 		dos.writeBytes("226 The file was succesfully sent!\n");
 	}
 
+	/**
+	 * Returns the list of files and directories in the client's current
+	 * directory.
+	 * 
+	 * @param argument
+	 *            unused
+	 * @throws IOException
+	 *             If an error occurs while writing on the data socket.
+	 */
 	protected void processLIST(final String argument) throws IOException {
 		dos = getOutputStream();
 		File directory = clientSession.getCurrentDirectory();
@@ -283,6 +345,15 @@ public class FtpRequest {
 		dos.writeBytes("200 \n");
 	}
 
+	/**
+	 * Builds the line to be sent to the client for a file upon a LIST request.
+	 * 
+	 * @param file
+	 *            The file in the directory
+	 * @return The line to be sent back to the client
+	 * @throws IOException
+	 *             If an error occurs while trying to read the file properties
+	 */
 	private String getListLine(File file) throws IOException {
 		String line = "";
 		PosixFileAttributes posixView = Files.readAttributes(file.toPath(),
@@ -359,6 +430,15 @@ public class FtpRequest {
 		return line;
 	}
 
+	/**
+	 * Stores a file on the server
+	 * 
+	 * @param file
+	 *            The file to store on the server
+	 * @throws IOException
+	 *             If an error occurs while trying to read form the data socket
+	 *             or creating the file.
+	 */
 	protected void processSTOR(final String file) throws IOException {
 		File newFile = new File(clientSession.getCurrentDirectory()
 				+ File.separator + file);
@@ -382,6 +462,14 @@ public class FtpRequest {
 		dos.writeBytes("226 Received and executed STOR " + file + " \n");
 	}
 
+	/**
+	 * Return bogus information about the server
+	 * 
+	 * @param argument
+	 *            unused
+	 * @throws IOException
+	 *             If an error occurs while writing on the data socket
+	 */
 	protected void processSYST(final String argument) throws IOException {
 		getOutputStream();
 		dos.writeBytes("215 UNIX Type: L8\n");
@@ -414,6 +502,15 @@ public class FtpRequest {
 		dos.writeBytes("200 " + message + "\n");
 	}
 
+	/**
+	 * Disconnect the client.
+	 * 
+	 * @param argument
+	 *            unused
+	 * @throws IOException
+	 *             If an error occurs while trying to close the connection or
+	 *             the notifiy the client that it has been disconnected.
+	 */
 	protected void processQUIT(final String argument) throws IOException {
 		dos = getOutputStream();
 		dos.writeBytes("221 Bye \n");
@@ -424,6 +521,15 @@ public class FtpRequest {
 			commandSocket.close();
 	}
 
+	/**
+	 * Write text on the data socket.
+	 * 
+	 * @param data
+	 *            Text to write
+	 * @return The data writtent on the socket
+	 * @throws IOException
+	 *             If an error occurs while trying to write on the data socket.
+	 */
 	private String writeOnDataSocket(final String data) throws IOException {
 		Socket dataSocket = getDataSocket();
 		DataOutputStream cos = new DataOutputStream(
